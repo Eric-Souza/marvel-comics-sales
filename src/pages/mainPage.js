ed7cd794599e915df97f04daedcd90fbe1ffb16c
Eric Souza <eric.bh18souza@gmail.com>
2020-12-10 22:16:30 -0300

    Project will be refactored

--- src/pages/mainPage.js
+++ src/pages/mainPage.js
@@ -1,16 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-import Products from '../components/products';
-import MarvelTitle from '../components/marvelTitle';
+// Services Import
+import api from '../services/api';
 
-const MainPage = () => {
-    
-    return (
-        <>
-            <MarvelTitle />
-            <Products />
-        </>
-    );
+export default class Products extends Component {
+    constructor() {
+        super()
+    };
+      
+    componentWillMount () {
+        this.showAllComics()
+    };
+      
+    showAllComics = async () => {
+        const apiKey = 'ts=1595879600&apikey=3e4689455ed92e49bc7607d9328ab0c6&hash=fbdacda12b4cb54ec2b3f1c51356fde6'
+        const response = await api.get(`/comics?limit=20&offset=${this.state.offset}&${apiKey}`)
+    };
+
+    render () {
+        return (
+            <div>
+                Olá Mundo
+            </div>
+        );
+    };
 };
-
-export default MainPage;
--- src/routes/appRoutes.js
+++ src/routes/appRoutes.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import {Redirect, BrowserRouter, Switch, Route} from 'react-router-dom';
+
+// React Routing Imports
+import { Redirect, BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import MainPage from '../pages/mainPage';
-import FinalPage from '../pages/finalPage';
 
 const Routes = () => (
     <BrowserRouter>
         <Switch>
             <Route exact path='/'      component={ MainPage } />
-            <Route exact path='/final' component={ FinalPage } />
             <Redirect from='*' to='/' />
         </Switch>
     </BrowserRouter>
